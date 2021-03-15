d3fc300d05512e448f308658de9ef43f74954165
jamie stollery <jmestollery@outlook.com>
2021-03-15 11:33:57 +0000

    add sprGetQuotes

---
+++ API/Database/dbo/Procedures/sprGetQuotes.sql
@@ -0,0 +1,12 @@
+﻿CREATE PROCEDURE [dbo].[sprGetQuotes]
+AS
+	SELECT
+		Q.QuoteID AS Id,
+		UQ.UserUN AS Username,
+		Q.QuoteCost AS TotalCost,
+		Q.QuoteDescription AS [Description],
+		QI.ItemID AS Id
+	From Quote Q
+	INNER JOIN QuoteItems QI ON Q.QuoteID = QI.QuoteID
+	INNER JOIN UserQuotes UQ ON Q.QuoteID = UQ.QuoteID
+RETURN
