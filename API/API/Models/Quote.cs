d63a33aa197dae7bf55ea4cb2ff9b26ca52a45b0
jamie stollery <jmestollery@outlook.com>
2021-03-15 21:26:52 +0000

    Quote request functionality

--- API/API/Models/Quote.cs
+++ API/API/Models/Quote.cs
@@ -1,10 +1,7 @@
 ﻿namespace API.Models
 {
-    public class Quote
+    public class Quote : QuoteRequest
     {
-        public string Id { get; set; }
-        public string ItemId { get; set; }
-        public string Username { get; set; }
         public float TotalCost { get; set; }
         public string Description { get; set; }
     }
