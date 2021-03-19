102589f2207e85e4280fe4c63f46b0e9c41a35d8
Chris <c.brewster@hotmail.co.uk>
2021-03-19 16:51:17 +0000

    Gets user info from API Endpoint and renders nav
    renders nav bar depending on account type from API

--- react-frontend/src/components/Nav.jsx
+++ react-frontend/src/components/Nav.jsx
@@ -1,19 +1,30 @@
 import Button from 'react-bootstrap/Button'
 import { useEffect, useState } from 'react'
+import axios from 'axios';
+
 
 export default function Nav(props) {
 
 
-    const [currentUser, setCurrent] = useState({})
+    const [userInfo, setInfo] = useState({})
 
     useEffect(()=>
     {
-        const user = props.user;
-        setCurrent(user)
-        console.log("Nav User: " + currentUser.username);
+        axios.get('user/info',{
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${localStorage.getItem('token')}`
+            },
+        })
+        .then((response) => {
+                console.log("Nav Effect: "+ response.data)
+                setInfo(response.data);
+            })  
+        .catch(err => console.log(err))
 
-
-    },[props.user.username])
+        // gets updated user info when new user logs in
+        // However this props.user RESET when page is refreshed.. as no data has been set from login
+    },[props.user])
 
 
     // Front End Routes
@@ -67,7 +78,7 @@ export default function Nav(props) {
     function VisitNav() {
         return (
             <div className="col text-right my-auto">
-                
+
                 <Button 
                 variant="primary" 
                 className="mr-2"
@@ -81,7 +92,7 @@ export default function Nav(props) {
 
     function DisplayNav() {
 
-        switch(currentUser.accountType)
+        switch(userInfo.accountType)
         {
             case 'E':
                 return ( <EmpNav /> )
