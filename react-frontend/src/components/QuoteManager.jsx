ef2f1b0555d1434d2295e499598d562fbc8183f9
llynskey <lawrence1910@googlemail.com>
2021-04-06 23:39:57 +0100

    Quote reply styling

--- react-frontend/src/components/QuoteManager.jsx
+++ react-frontend/src/components/QuoteManager.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import { Row, Col } from 'react-bootstrap';
 import AcceptQuote from '../components/QuoteManagerButtons/AcceptQuote';
 import RejectQuote from '../components/QuoteManagerButtons/RejectQuote';
 
@@ -16,58 +17,63 @@ export default function QuoteManager(props) {
 
         const itemId = props.itemId;
 
-        axios.get(`CatalogItem/${itemId}`,{
+        axios.get(`CatalogItem/${itemId}`, {
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${localStorage.getItem('token')}`
             },
         })
-        .then((response) => {
+            .then((response) => {
 
-            setItem(response.data)
-        })
-        .catch(err => console.log(err))        
-    
-    },[props.id]) // effect is only called when the request id changes
+                setItem(response.data)
+            })
+            .catch(err => console.log(err))
+
+    }, [props.id]) // effect is only called when the request id changes
 
     function LoadButtons() {
-        if(quote.status === "PROCESSED") {
+        if (quote.status === "PROCESSED") {
             return (
-                <div>
-                    <AcceptQuote {...quote}/>
-                    <RejectQuote {...quote}/>
-              
-                 </div>
+                <div style={{width:"50%", margin:"auto", paddingBottom:"20px"}}>
+                    <AcceptQuote {...quote} />
+                    <RejectQuote {...quote} />
+
+                </div>
 
             )
-        } else { return ( <div></div> ) }
-       
+        } else { return (<div></div>) }
+
     }
 
-    return (    
-   
-        <div className="Wrapper"> 
-            <h3>Quote Details</h3>
-            <div className="ItemBox">
-                <small style={{fontWeight: 'bold'}}>Item Name</small>
-                <p className="">{item.name}</p>
-                <small style={{fontWeight: 'bold'}}>Description</small>
-                <p className="">{item.description}</p>
-                <small style={{fontWeight: 'bold'}}>Supplier</small>
-                <p className="">{item.supplierName}</p>
-            </div>
+    return (
 
-            <div className="QuoteBox">
-                <small style={{fontWeight: 'bold'}}>Cost</small>
-                <p className="">{quote.totalCost}</p>
-                <small style={{fontWeight: 'bold'}}>Description</small>
-                <p className="">{quote.description}</p>
- 
-            {/*Renders button to accept quote if the request has been processed*/}
-            <LoadButtons />
-           
-            </div>
+        <div className="Wrapper">
+            <Row>
+                <Col>
+                <h3>Quote Details</h3>
+                <div className="ItemBox">
+                    <small style={{ fontWeight: 'bold' }}>Item Name</small>
+                    <p className="">{item.name}</p>
+                    <small style={{ fontWeight: 'bold' }}>Description</small>
+                    <p className="">{item.description}</p>
+                    <small style={{ fontWeight: 'bold' }}>Supplier</small>
+                    <p className="">{item.supplierName}</p>
+                </div>
+                </Col>
+                <Col>
+                <div className="ItemBox">
+                    <small style={{ fontWeight: 'bold' }}>Cost</small>
+                    <p className="">{quote.totalCost}</p>
+                    <small style={{ fontWeight: 'bold' }}>Description</small>
+                    <p className="">{quote.description}</p>
+
+                    {/*Renders button to accept quote if the request has been processed*/}
+                    </div>
+                    </Col>
+            </Row>
+                    <LoadButtons />
+
         </div>
 
-    ); 
+    );
 }
